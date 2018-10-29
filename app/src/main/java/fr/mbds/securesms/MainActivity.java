53c0e2f09c6de6e669153a93c4f37fbad584a1c4
GraceBK <iris.b1991@gmail.com>
2018-10-29 17:26:34 +0100

    aa

--- app/src/main/java/fr/mbds/securesms/MainActivity.java
+++ app/src/main/java/fr/mbds/securesms/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
@@ -14,19 +13,27 @@ import fr.mbds.securesms.fragments.ListContactFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    FrameLayout frameLayout;
+    FrameLayout fl_list;
+    FrameLayout fl_chat;
     FloatingActionButton floatingActionButton;
 
     ChatFragment chatFragment = new ChatFragment();
     ListContactFragment listContactFragment = new ListContactFragment();
 
     boolean a = true;
+    boolean isLand = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fl_list = findViewById(R.id.main_fl_list);
+        fl_chat = findViewById(R.id.main_fl_chat);
 
+        int orientation = getResources().getConfiguration().orientation;
+
+        updateDisplay(orientation);
+/*
         frameLayout = findViewById(R.id.main_frame_layout);
         floatingActionButton = findViewById(R.id.fab);
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
@@ -43,18 +50,34 @@ public class MainActivity extends AppCompatActivity {
                 fragmentTransaction.commit();
             }
         });
-
+*/
     }
+
+    private void updateDisplay(int orientation)
+    {
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE)
+        {
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            Toast.makeText(this, "Landscape", Toast.LENGTH_SHORT).show();
+            fragmentTransaction.replace(fl_list.getId(), listContactFragment);
+            fragmentTransaction.replace(fl_chat.getId(), chatFragment);
+            fragmentTransaction.commit();
+        }
+        else
+        {
+            Toast.makeText(this, "Portrait", Toast.LENGTH_SHORT).show();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(fl_list.getId(), listContactFragment);
+            fragmentTransaction.commit();
+        }
+    }
+
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
 
         // Checks the orientation of the screen
-        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            Toast.makeText(this, "Landscape", Toast.LENGTH_SHORT).show();
-        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            Toast.makeText(this, "Portrait", Toast.LENGTH_SHORT).show();
-        }
+        updateDisplay(newConfig.orientation);
     }
 }
