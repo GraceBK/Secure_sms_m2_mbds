68f24252fb1b49c3a6f13f3d57e6337d95f9316e
GraceBK <iris.b1991@gmail.com>
2018-10-29 12:48:16 +0100

    // TODO revoir l'Orientation

--- app/src/main/java/fr/mbds/securesms/MainActivity.java
+++ app/src/main/java/fr/mbds/securesms/MainActivity.java
@@ -1,7 +1,9 @@
 package fr.mbds.securesms;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // Checks the orientation of the screen
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Toast.makeText(this, "Landscape", Toast.LENGTH_SHORT).show();
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Toast.makeText(this, "Portrait", Toast.LENGTH_SHORT).show();
+        }
     }
 }
