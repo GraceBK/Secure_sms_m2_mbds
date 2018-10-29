1fe9a6bcdfd5502041967ede01126ddb12197423
GraceBK <iris.b1991@gmail.com>
2018-10-29 15:39:42 +0100

    Ajout des Fragments

--- app/src/main/java/fr/mbds/securesms/MainActivity.java
+++ app/src/main/java/fr/mbds/securesms/MainActivity.java
@@ -1,16 +1,49 @@
 package fr.mbds.securesms;
 
 import android.content.res.Configuration;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import fr.mbds.securesms.fragments.ChatFragment;
+import fr.mbds.securesms.fragments.ListContactFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    FrameLayout frameLayout;
+    FloatingActionButton floatingActionButton;
+
+    ChatFragment chatFragment = new ChatFragment();
+    ListContactFragment listContactFragment = new ListContactFragment();
+
+    boolean a = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frameLayout = findViewById(R.id.main_frame_layout);
+        floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                a = !a;
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                if (a) {
+                    fragmentTransaction.replace(frameLayout.getId(), listContactFragment);
+                } else {
+                    fragmentTransaction.replace(frameLayout.getId(), chatFragment);
+                }
+
+                fragmentTransaction.commit();
+            }
+        });
+
     }
 
     @Override
