7ff1d8b1959fd995b5fdac1c275bf62859de8dca
GraceBK <iris.b1991@gmail.com>
2018-11-08 16:17:40 +0100

    Correction de l'erreur 400 avec l'enregistrement

--- app/src/main/java/fr/mbds/securesms/RegisterActivity.java
+++ app/src/main/java/fr/mbds/securesms/RegisterActivity.java
@@ -9,23 +9,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
-import java.util.HashMap;
-import java.util.Map;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import fr.mbds.securesms.utils.MyURL;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    EditText et_name;
+    EditText et_confirm_password;
     EditText et_username;
     EditText et_password;
     Button btn_create;
@@ -41,9 +41,14 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void register() {
-        if (!et_username.getText().toString().equals("") && !et_password.getText().toString().equals("")) {
-            btn_create.setBackgroundColor(Color.GREEN);
-            requestCreateUser(et_username.getText().toString(), et_password.getText().toString());
+        if (!et_username.getText().toString().equals("") && !et_password.getText().toString().equals("")
+                && !et_confirm_password.getText().toString().equals("")) {
+            if (et_password.getText().toString().equals(et_confirm_password.getText().toString())) {
+                btn_create.setBackgroundColor(Color.GREEN);
+                requestCreateUser(et_username.getText().toString(), et_password.getText().toString());
+            } else {
+                Toast.makeText(this, "Password different", Toast.LENGTH_SHORT).show();
+            }
         } else {
             btn_create.setBackgroundColor(Color.RED);
         }
@@ -53,11 +58,19 @@ public class RegisterActivity extends AppCompatActivity {
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
 
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, MyURL.SIGNUP.toString(),
-                new Response.Listener<String>() {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("username", username);
+            jsonObject.put("password", password);
+        } catch (JSONException e) {
+            Log.e("ERROR JSONObject", jsonObject.toString());
+        }
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.POST, MyURL.SIGNUP.toString(), jsonObject,
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(String response) {
-                        Log.i("REGISTER", response);
+                    public void onResponse(JSONObject response) {
+                        Log.i("REGISTER", response.toString());
                         // TODO : Generer Private and Public key
                         Intent goToMain = new Intent(RegisterActivity.this, MainActivity.class);
                         startActivity(goToMain);
@@ -67,20 +80,13 @@ public class RegisterActivity extends AppCompatActivity {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "Contact existant", Toast.LENGTH_SHORT).show();
+                        clearAllEditText();
                         Log.i("ERROR REGISTER", error.toString());
                     }
-                })
-        {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<>();
-                params.put("username", username);
-                params.put("password", password);
-                return params;
-            }
-        };
-        queue.add(stringRequest);
-        Log.e("REGISTER", ""+stringRequest.getUrl());
+                });
+        queue.add(objectRequest);
+        Log.e("REGISTER", ""+objectRequest.getUrl());
     }
 
     public void actionComposant() {
@@ -102,10 +108,16 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+    public void clearAllEditText() {
+        et_username.getText().clear();
+        et_password.getText().clear();
+        et_confirm_password.getText().clear();
+    }
+
     public void initComposant() {
-        et_name = findViewById(R.id.register_et_name);
         et_username = findViewById(R.id.register_et_username);
         et_password = findViewById(R.id.register_et_password);
+        et_confirm_password = findViewById(R.id.register_et_password2);
         tv_go_login = findViewById(R.id.register_tv_go_login);
         btn_create = findViewById(R.id.register_btn_create);
     }
