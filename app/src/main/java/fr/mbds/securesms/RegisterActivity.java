c6d1666d7aff1239410098b646413244ca51142a
GraceBK <iris.b1991@gmail.com>
2018-12-30 15:56:48 +0100

    Update RegisterActivity.java

--- app/src/main/java/fr/mbds/securesms/RegisterActivity.java
+++ app/src/main/java/fr/mbds/securesms/RegisterActivity.java
@@ -1,9 +1,8 @@
 package fr.mbds.securesms;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +19,9 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import fr.mbds.securesms.utils.MyURL;
 
@@ -44,13 +46,14 @@ public class RegisterActivity extends AppCompatActivity {
         if (!et_username.getText().toString().equals("") && !et_password.getText().toString().equals("")
                 && !et_confirm_password.getText().toString().equals("")) {
             if (et_password.getText().toString().equals(et_confirm_password.getText().toString())) {
-                btn_create.setBackgroundColor(Color.GREEN);
+                //btn_create.setBackgroundColor(Color.GREEN);
                 requestCreateUser(et_username.getText().toString(), et_password.getText().toString());
             } else {
                 Toast.makeText(this, "Password different", Toast.LENGTH_SHORT).show();
             }
         } else {
-            btn_create.setBackgroundColor(Color.RED);
+            //btn_create.setBackgroundColor(Color.RED);
+            Toast.makeText(getApplicationContext(), "Champs vide", Toast.LENGTH_LONG).show();
         }
     }
 
@@ -63,14 +66,14 @@ public class RegisterActivity extends AppCompatActivity {
             jsonObject.put("username", username);
             jsonObject.put("password", password);
         } catch (JSONException e) {
-            Log.e("ERROR JSONObject", jsonObject.toString());
+            Log.e("[ERROR JSONObject]", jsonObject.toString());
         }
 
         JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.POST, MyURL.CREATE_USER.toString(), jsonObject,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        Log.i("REGISTER", response.toString());
+                        Log.i("[REGISTER]", response.toString());
                         // TODO : Generer Private and Public key
                         // TODO : Save current state (is connect) in SharePreference
                         Intent goToMain = new Intent(RegisterActivity.this, MainActivity.class);
@@ -83,9 +86,19 @@ public class RegisterActivity extends AppCompatActivity {
                     public void onErrorResponse(VolleyError error) {
                         Toast.makeText(getApplicationContext(), "Contact existant", Toast.LENGTH_SHORT).show();
                         clearAllEditText();
-                        Log.e("ERROR REGISTER", error.toString());
+                        Log.e("[ERROR REGISTER]", error.toString());
                     }
-                });
+                }
+        )
+        {
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+        };
         queue.add(objectRequest);
     }
 
