d5717bab53ecd85d305be5868bf3df9500c4764b
GraceBK <iris.b1991@gmail.com>
2019-01-05 12:44:53 +0100

    Creation d'un BroadCast

---
+++ app/src/main/java/fr/mbds/securesms/broadcast/MyBroadcast.java
@@ -0,0 +1,21 @@
+package fr.mbds.securesms.broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import fr.mbds.securesms.service.MyServiceFetchMessage;
+
+public class MyBroadcast extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+
+            Log.i(MyBroadcast.class.getSimpleName(), "Service Start");
+
+            Intent intent1 = new Intent(context, MyServiceFetchMessage.class);
+            context.startService(intent1);
+        }
+    }
+}
